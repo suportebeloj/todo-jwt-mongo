3a244e773b28e33f48973333644622e129d88ae3
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-24 16:34:33 -0300

    initial interface from users

---
+++ internal/ports/database.go
@@ -0,0 +1,12 @@
+package ports
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"todo-jwt-mongo/internal/authentication/models"
+)
+
+type UserDatabasePort interface {
+	Save(ctx context.Context, user models.UserData) (*primitive.ObjectID, error)
+	GetByName(ctx context.Context, username string) (*models.User, error)
+}
