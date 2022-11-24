c370801c551c095ceac78e07c8a1011591c8ff59
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-24 12:37:57 -0300

    create hash password and validate password functions

---
+++ internal/authentication/crypt.go
@@ -0,0 +1,25 @@
+package authentication
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"math/rand"
+	"todo-jwt-mongo/internal/authentication/models"
+)
+
+func hashPassword(user models.RegisterUser) (hashedPass string, salt int, err error) {
+	salt = rand.Int()
+	plainStr := fmt.Sprintf("%s.%s.%d.%s", user.Username, user.Email, salt, user.Password)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(plainStr), 14)
+	if err != nil {
+		return "", 0, err
+	}
+
+	return string(bytes), salt, err
+}
+
+func validatePassword(plainPassword string, user *models.User) bool {
+	plainStr := fmt.Sprintf("%s.%s.%d.%s", user.Username, user.Profile.Email, user.Salt, plainPassword)
+	err := bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(plainStr))
+	return err == nil
+}
