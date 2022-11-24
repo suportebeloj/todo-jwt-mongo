f3476b08ac188bf97fadd6299665b16f0ab096eb
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-24 13:45:21 -0300

    model userData is used to save data without inform ID

--- internal/authentication/models/users.go
+++ internal/authentication/models/users.go
@@ -23,6 +23,16 @@ type User struct {
 	DeletedAt      time.Time          `json:"deleted_at" bson:"deleted_at"`
 }
 
+type UserData struct {
+	Username       string    `json:"username" bson:"username"`
+	HashedPassword string    `json:"hashed_password" bson:"hashed_password"`
+	Salt           int       `json:"salt" bson:"salt"`
+	Profile        Profile   `json:"profile" bson:"profile"`
+	CreatedAt      time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at" bson:"updated_at"`
+	DeletedAt      time.Time `json:"deleted_at" bson:"deleted_at"`
+}
+
 type AuthUser struct {
 	Username string `json:"username" bson:"username"`
 	Password string `json:"password" bson:"password"`
