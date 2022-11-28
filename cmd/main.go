83e3207f8ac36e0fe45471fc4211e112486bf26b
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-28 17:08:39 -0300

    function to setup database

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,19 @@ func main() {
 	httpServerSetup()
 }
 
-func httpServerSetup() {
+func SetupMongoDB() (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	client, err := database.NewMongoDBClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func httpServerSetup(db *mongo.Client) {
 	fiberApp := fiber.New()
 	fiberApp.Use(logger.New())
 
